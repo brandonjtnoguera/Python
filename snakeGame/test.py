52d9d7a5cdafc583deec014ac56948a42facc2bd
Brandon <bran.ten25@gmail.com>
2022-01-30 14:52:03 -0500

    initial commit

--- snakeGame/test.py
+++ snakeGame/test.py
@@ -1,22 +1,21 @@
-"""
-https://www.geeksforgeeks.org/create-a-snake-game-using-turtle-in-python/
-"""
-import turtle, time, random
+# import required modules
+import turtle
+import time
+import random
 
 delay = 0.1
 score = 0
 high_score = 0
 
-# Creates a window screen
+# Creating a window screen
 wn = turtle.Screen()
 wn.title("Snake Game")
-wn.bgcolor("black")
-
-# Setting width and height of the screen
+wn.bgcolor("blue")
+# the width and height can be put as user's choice
 wn.setup(width=600, height=600)
 wn.tracer(0)
 
-# Creating head of the snake
+# head of the snake
 head = turtle.Turtle()
 head.shape("square")
 head.color("white")
@@ -24,10 +23,10 @@ head.penup()
 head.goto(0, 0)
 head.direction = "Stop"
 
-# Food in the game
+# food in the game
 food = turtle.Turtle()
-colors = random.choice(["red", "green", "yellow"])
-shapes = random.choice(["circle", "square", "triangle"])
+colors = random.choice(['red', 'green', 'black'])
+shapes = random.choice(['square', 'triangle', 'circle'])
 food.speed(0)
 food.shape(shapes)
 food.color(colors)
@@ -41,24 +40,30 @@ pen.color("white")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 250)
-pen.write("Score : 0 High Score : 0", align="center", font=("candria", 24, 'bold'))
+pen.write("Score : 0  High Score : 0", align="center",
+          font=("candara", 24, "bold"))
 
-# Assigning key directions
-def go_up():
+
+# assigning key directions
+def goup():
     if head.direction != "down":
         head.direction = "up"
 
-def go_down():
+
+def godown():
     if head.direction != "up":
         head.direction = "down"
 
-def go_left():
+
+def goleft():
     if head.direction != "right":
         head.direction = "left"
 
-def go_right():
+
+def goright():
     if head.direction != "left":
         head.direction = "right"
+
 
 def move():
     if head.direction == "up":
@@ -67,22 +72,23 @@ def move():
     if head.direction == "down":
         y = head.ycor()
         head.sety(y - 20)
-    if head.direction == "right":
-        x = head.xcor()
-        head.setx(x + 20)
     if head.direction == "left":
         x = head.xcor()
         head.setx(x - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
 
 wn.listen()
-wn.onkeypress(go_up, "w")
-wn.onkeypress(go_left, "a")
-wn.onkeypress(go_down, "s")
-wn.onkeypress(go_right, "d")
+wn.onkeypress(goup, "w")
+wn.onkeypress(godown, "s")
+wn.onkeypress(goleft, "a")
+wn.onkeypress(goright, "d")
 
 segments = []
 
-# Main gameplay
+# Main Gameplay
 while True:
     wn.update()
     if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
@@ -90,24 +96,25 @@ while True:
         head.goto(0, 0)
         head.direction = "Stop"
         colors = random.choice(['red', 'blue', 'green'])
-        shapes = random.choice(['circle', 'square'])
+        shapes = random.choice(['square', 'circle'])
         for segment in segments:
             segment.goto(1000, 1000)
         segments.clear()
         score = 0
         delay = 0.1
         pen.clear()
-        pen.write(f"Score : {score} High score: {high_score}", align="center", font=("candara", 24, "bold"))
+        pen.write("Score : {} High Score : {} ".format(
+            score, high_score), align="center", font=("candara", 24, "bold"))
     if head.distance(food) < 20:
         x = random.randint(-270, 270)
         y = random.randint(-270, 270)
         food.goto(x, y)
 
-        # Adding segments
+        # Adding segment
         new_segment = turtle.Turtle()
         new_segment.speed(0)
         new_segment.shape("square")
-        new_segment.color("white")  # Color of tail
+        new_segment.color("orange")  # tail colour
         new_segment.penup()
         segments.append(new_segment)
         delay -= 0.001
@@ -115,9 +122,9 @@ while True:
         if score > high_score:
             high_score = score
         pen.clear()
-        pen.write(f"Score : {score} High score: {high_score}", align="center", font=("candara", 24, "bold"))
-
-        # Checking for head collisions with body
+        pen.write("Score : {} High Score : {} ".format(
+            score, high_score), align="center", font=("candara", 24, "bold"))
+    # Checking for head collisions with body segments
     for index in range(len(segments) - 1, 0, -1):
         x = segments[index - 1].xcor()
         y = segments[index - 1].ycor()
@@ -131,17 +138,18 @@ while True:
         if segment.distance(head) < 20:
             time.sleep(1)
             head.goto(0, 0)
-            head.direction = "Stop"
+            head.direction = "stop"
             colors = random.choice(['red', 'blue', 'green'])
-            shapes = random.choice(['circle', 'square'])
+            shapes = random.choice(['square', 'circle'])
             for segment in segments:
                 segment.goto(1000, 1000)
-            segments.clear()
+            segment.clear()
 
             score = 0
             delay = 0.1
             pen.clear()
-            pen.write(f"Score : {score} High score: {high_score}", align="center", font=("candara", 24, "bold"))
-            time.sleep(delay)
-wn.mainLoop()
+            pen.write("Score : {} High Score : {} ".format(
+                score, high_score), align="center", font=("candara", 24, "bold"))
+    time.sleep(delay)
+wn.mainloop()
 
